82088ce525c9de4b6a1eddde853727e1a805a0e4
Niozk <99679240+Niozk@users.noreply.github.com>
2024-06-19 15:58:38 +0200

    Directory change and nav template added
    -Added some folders

---
+++ lib/telze_home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class TelzeHomePage extends StatefulWidget {
+  const TelzeHomePage({super.key});
+
+  @override
+  State<TelzeHomePage> createState() => _TelzeHomePageState();
+}
+
+class _TelzeHomePageState extends State<TelzeHomePage> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Page 1')),
+    Center(child: Text('Page 2')),
+    Center(child: Text('Page 3')),
+    Center(child: Text('Page 4')),
+    Center(child: Text('Page 5')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.animateToPage(
+        _selectedIndex,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Telze'),
+        backgroundColor: Colors.green,
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: _pages,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
